83d1bdb12df555a72f9621fb3e19bddcdaec4d3d
Andrii Kvach <0968512988z@gmail.com>
2021-08-24 22:10:40 +0300

    auth error handling

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
   logout: 'Вы вишли из системы',
+  'auth/user-not-found': 'Пользователя с таким email не существует',
+  'auth/wrong-password': 'Не верный пароль',
+  'auth/email-already-in-use': 'Email уже занят',
 };
