26f2809838e7428bdad71ba38cc6bbdbea74b387
Andrii Kvach <0968512988z@gmail.com>
2021-08-24 14:37:05 +0300

    connect firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -14,8 +17,24 @@ Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: 'AIzaSyDPyhYFQZjiTLYJ8iAb34wjDr2yCJ23PDo',
+  authDomain: 'vue-crm-f41d9.firebaseapp.com',
+  projectId: 'vue-crm-f41d9',
+  storageBucket: 'vue-crm-f41d9.appspot.com',
+  messagingSenderId: '601466060370',
+  appId: '1:601466060370:web:208134eb1c842203dfe98b',
+  measurementId: 'G-SLTED14BRR',
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount('#app');
+  }
+});
