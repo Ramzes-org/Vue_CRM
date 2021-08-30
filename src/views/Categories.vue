d80647de245e4a09b78e936643c1e10f293caec0
Andrii Kvach <0968512988z@gmail.com>
2021-08-31 00:40:56 +0300

    feat(categories): add action fetchCategories

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,7 +4,8 @@
       <h3>Категории</h3>
     </div>
     <section>
-      <div class="row">
+      <Loader v-if="loading" />
+      <div class="row" v-else>
         <CategoryCreate @created="addNewCategory" />
         <CategoryEdit />
       </div>
@@ -20,14 +21,18 @@ export default {
   name: 'categories',
   data: () => ({
     categories: [],
+    loading: true,
   }),
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories');
+    this.loading = false;
+  },
   components: {
     CategoryCreate, CategoryEdit,
   },
   methods: {
     addNewCategory(category) {
       this.categories.push(category);
-      console.log(this.categories);
     },
   },
 };
