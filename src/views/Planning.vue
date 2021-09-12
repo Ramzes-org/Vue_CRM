fd82b718ab8b3bba2d9727b3914d2022fcf882e3
Andrii Kvach <0968512988z@gmail.com>
2021-09-12 23:52:09 +0300

    feat(planing): add data()

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -5,7 +5,11 @@
       <h4>12 212</h4>
     </div>
 
-    <section>
+    <loader v-if="loading" />
+    <p class="center" v-else-if="!categories.length">
+      Категорий пока нет. <router-link to="/categories">Добавить новую категорию</router-link>
+    </p>
+    <section v-else>
       <div>
         <p>
           <strong>Девушка:</strong>
@@ -18,3 +22,19 @@
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'planing',
+  data: () => ({
+    loadig: true,
+    categories: [],
+  }),
+  async mounted() {
+    const records = await this.$store.dispatch('fethRecords');
+    const categories = await this.$store.dispatch('fetchCategories');
+    console.log(records, categories);
+    this.loadig = false;
+  },
+};
+</script>
